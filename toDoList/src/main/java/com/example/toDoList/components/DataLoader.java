973195aad24ba41e2c1d815c6f4b0514a55493a9
Ryder Rashid <ryderzane@Ryders-MacBook-Air-2.local>
2023-02-21 16:12:47 +0000

    finished data-loader and enum

--- toDoList/src/main/java/com/example/toDoList/components/DataLoader.java
+++ toDoList/src/main/java/com/example/toDoList/components/DataLoader.java
@@ -1,5 +1,6 @@
 package com.example.toDoList.components;
 
+import com.example.toDoList.models.ListCategory;
 import com.example.toDoList.models.ToDo;
 import com.example.toDoList.models.ToDoList;
 import com.example.toDoList.models.User;
@@ -27,7 +28,7 @@ public class DataLoader implements ApplicationRunner {
     UserRepository userRepository;
 
 
-    public DataLoader(){
+    public DataLoader() {
 
     }
 
@@ -35,61 +36,86 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
 
-      //Amy
+        //Amy
         //Household
-    User amy = new User("Amy" , "amy@gmail.com", new ArrayList<>());
-    userRepository.save(amy);
+        User amy = new User("Amy", "amy@gmail.com", new ArrayList<>());
+        userRepository.save(amy);
 
-     //create a shopping list
-    ToDoList shoppingList = new ToDoList("Shopping List", new ArrayList<>(), amy);
-    toDoListRepository.save(shoppingList);
+        //create a shopping list
+        ToDoList shoppingList = new ToDoList("Shopping List", new ArrayList<>(), amy, ListCategory.HOUSEHOLD);
+        toDoListRepository.save(shoppingList);
 
-     //create a to-do
-     ToDo paint = new ToDo("Buy some paints" , "For house" , "26th Feb 2023" , shoppingList);
-          toDoRepository.save(paint);
+        //create a to-do
+        ToDo paint = new ToDo("Buy some paints", "For house", "26th Feb 2023", shoppingList);
+        toDoRepository.save(paint);
 
-     ToDo chair = new ToDo("Buy 4 chairs" , "For house" , "26th Feb 2023" , shoppingList);
-      toDoRepository.save(chair);
+        ToDo chair = new ToDo("Buy 4 chairs", "For house", "26th Feb 2023", shoppingList);
+        toDoRepository.save(chair);
 
-     ToDo curtain = new ToDo("Buy 4 chairs" , "For house" , "26th Feb 2023" , shoppingList);
-     toDoRepository.save(chair);
+        ToDo curtain = new ToDo("Buy curtains", "For house", "26th Feb 2023", shoppingList);
+        toDoRepository.save(curtain);
+
+        ToDo tomatoes = new ToDo("Buy some tomatoes", "For dinner", "28th Feb 2023", shoppingList);
+        toDoRepository.save(tomatoes);
+
+        ToDo eggs = new ToDo("Buy eggs", "For dinner", "28th Feb 2023", shoppingList);
+        toDoRepository.save(tomatoes);
+
+        //Health
+
+        //create a list
+        ToDoList journal = new ToDoList("Journal", new ArrayList<>(), amy, ListCategory.HEALTH);
+        toDoListRepository.save(journal);
+
+        //create a to-do
+        ToDo writeEntry = new ToDo("Write gratitude", "For the day", "Daily", shoppingList);
+        toDoRepository.save(writeEntry);
+
+        //Self-care
+        //create a list
+        ToDoList moodBoard = new ToDoList("My mood board", new ArrayList<>(), amy, ListCategory.SELFCARE);
+        toDoListRepository.save(moodBoard);
+
+        //create a to-do
+        ToDo getCanvas = new ToDo("layout pictures for board", "for vision board", "3rd Mar 2023", moodBoard);
+        toDoRepository.save(getCanvas);
 
 
-     ToDo tomatoes = new ToDo("Buy some tomatoes" , "For dinner" , "28th Feb 2023" , shoppingList);
-     toDoRepository.save(tomatoes);
+        //Work
 
-     ToDo eggs = new ToDo("Buy eggs" , "For dinner" , "28th Feb 2023" , shoppingList);
-          toDoRepository.save(tomatoes);
+        //create a list
+        ToDoList launchWebsite = new ToDoList("Launch website", new ArrayList<>(), amy, ListCategory.WORK);
+        toDoListRepository.save(launchWebsite);
 
-     //
+        //create a to-do
+        ToDo createwebsite = new ToDo("Create Html", "For site", "Monday", shoppingList);
+        toDoRepository.save(createwebsite);
 
-
-
-     //User Tom
+        //User Tom
 
         //Self Care
 
         User tom = new User("Tom", "tom@gmail.com", new ArrayList<>());
         userRepository.save(tom);
 
-        ToDoList morningRoutine = new ToDoList("morningRoutine", new ArrayList<>(), tom);
+        ToDoList morningRoutine = new ToDoList("morningRoutine", new ArrayList<>(), tom, ListCategory.SELFCARE);
         toDoListRepository.save(morningRoutine);
 
-        ToDo wakeUp = new ToDo("wake up 7am" , "Journal and skincare" , "daily" , morningRoutine);
+        ToDo wakeUp = new ToDo("wake up 7am", "Journal and skincare", "daily", morningRoutine);
         toDoRepository.save(wakeUp);
 
         //Work
-        ToDoList presentation  = new ToDoList("presentation", new ArrayList<>(), tom);
+        ToDoList presentation = new ToDoList("presentation", new ArrayList<>(), tom, ListCategory.WORK);
         toDoListRepository.save(presentation);
 
-        ToDo prepare = new ToDo("Java Class" , "create powerpoint" , "Friday" , presentation);
+        ToDo prepare = new ToDo("Java Class", "create powerpoint", "Friday", presentation);
         toDoRepository.save(prepare);
 
         //Health
-        ToDoList workoutRoutine = new ToDoList("Sleep", new ArrayList<>(), tom);
+        ToDoList workoutRoutine = new ToDoList("Sleep", new ArrayList<>(), tom, ListCategory.HEALTH);
         toDoListRepository.save(workoutRoutine);
 
-        ToDo getUp = new ToDo("sleep" , "end the day with me" , "Daily",workoutRoutine);
+        ToDo getUp = new ToDo("sleep", "end the day with me", "Daily", workoutRoutine);
         toDoRepository.save(getUp);
 
 
@@ -100,17 +126,17 @@ public class DataLoader implements ApplicationRunner {
         User katy = new User("Katy", "katy@gmail.com", new ArrayList<>());
         userRepository.save(katy);
 
-        ToDoList studyPlan = new ToDoList("Study Plan", new ArrayList<>(), katy);
+        ToDoList studyPlan = new ToDoList("Study Plan", new ArrayList<>(), katy, ListCategory.WORK);
         toDoListRepository.save(studyPlan);
 
-        ToDo revisionguide = new ToDo("study" , "Create study guide" , "daily",studyPlan );
+        ToDo revisionguide = new ToDo("study", "Create study guide", "daily", studyPlan);
         toDoRepository.save(revisionguide);
 
         // Household
-        ToDoList plantTracker = new ToDoList("Plant Tracker", new ArrayList<>(), katy);
+        ToDoList plantTracker = new ToDoList("Plant Tracker", new ArrayList<>(), katy, ListCategory.HOUSEHOLD);
         toDoListRepository.save(plantTracker);
 
-        ToDo waterPlant = new ToDo("plant check" , "Water plants" , "every other day",plantTracker );
+        ToDo waterPlant = new ToDo("plant check", "Water plants", "every other day", plantTracker);
         toDoRepository.save(waterPlant);
 
 
@@ -121,41 +147,28 @@ public class DataLoader implements ApplicationRunner {
         User mike = new User("Mike", "mike@gmail.com", new ArrayList<>());
         userRepository.save(mike);
 
-        ToDoList meetingSchedule = new ToDoList("Meeting", new ArrayList<>(), mike);
+        ToDoList meetingSchedule = new ToDoList("Meeting", new ArrayList<>(), mike, ListCategory.WORK);
         toDoListRepository.save(meetingSchedule);
 
-        ToDo setMeeting = new ToDo("meeting" , "Schedule meeting with Kate" , "Thursday",meetingSchedule);
+        ToDo setMeeting = new ToDo("meeting", "Schedule meeting with Kate", "Thursday", meetingSchedule);
         toDoRepository.save(setMeeting);
 
         //Self Care
-        ToDoList eveningRoutine = new ToDoList("Sleep", new ArrayList<>(), mike);
+        ToDoList eveningRoutine = new ToDoList("Sleep", new ArrayList<>(), mike, ListCategory.SELFCARE);
         toDoListRepository.save(eveningRoutine);
 
-        ToDo unWind = new ToDo("sleep" , "end the day with me" , "Daily",eveningRoutine);
+        ToDo unWind = new ToDo("sleep", "end the day with me", "Daily", eveningRoutine);
         toDoRepository.save(unWind);
 
         //Household
-        ToDoList cleanHouse = new ToDoList("Clean", new ArrayList<>(), mike);
+        ToDoList cleanHouse = new ToDoList("Clean", new ArrayList<>(), mike, ListCategory.HOUSEHOLD);
         toDoListRepository.save(cleanHouse);
 
-        ToDo tidyUp = new ToDo("tidyUp" , "clean up house every weekend" , "Saturday",cleanHouse);
+        ToDo tidyUp = new ToDo("tidyUp", "clean up house every weekend", "Saturday", cleanHouse);
         toDoRepository.save(tidyUp);
 
 
-
-
-
-
-
-
-
-
-
     }
-
-
-
-
 
 
 }
