4cd926c2e5dae772a21bd14c6fc9596e9b935c7d
XixianWei <xixian.wei94@gmail.com>
2023-02-21 10:40:00 +0000

    second edited mapped by

--- toDoList/src/main/java/com/example/toDoList/models/User.java
+++ toDoList/src/main/java/com/example/toDoList/models/User.java
@@ -22,7 +22,7 @@ public class User {
     private String email;
 
     @JsonIgnoreProperties({"users"})
-    @OneToMany
+    @OneToMany(mappedBy = "user")
     private List<ToDoList> toDoLists;
 
     //constructors
